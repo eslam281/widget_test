5a9fcd56de55f4f20ba569dc4a411429b7d3c27d
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-10-17 22:26:24 +0300

    animation widget test first test start finsh

--- test/animation_screen_test.dart
+++ test/animation_screen_test.dart
@@ -1,3 +1,37 @@
-void main() {
 
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:widget_test/animation/animation_screen.dart';
+
+void main() {
+testWidgets(
+    "AnimationScreen Widget Test",
+      (widgetTester) async{
+      await widgetTester.pumpWidget(MaterialApp(home: AnimationScreen(),));
+
+      final containerFinder = find.byType(Container);
+      expect(containerFinder, findsOneWidget);
+
+      var container = widgetTester.widget(containerFinder) as Container;
+
+      // INITIAL
+      expect(container.constraints?.maxWidth,50);
+      expect(container.constraints?.maxHeight,50);
+      expect((container.decoration as BoxDecoration).color,Colors.blue);
+      expect((container.decoration as BoxDecoration).borderRadius,BorderRadius.zero);
+      
+      //
+        await widgetTester.pumpAndSettle();
+       container = widgetTester.widget(containerFinder) as Container;
+
+      expect(container.constraints?.maxWidth,200);
+      expect(container.constraints?.maxHeight,200);
+      expect((container.decoration as BoxDecoration).color,Colors.green);
+      expect((container.decoration as BoxDecoration).borderRadius,BorderRadius.circular(50));
+
+
+    },
+);
 }
